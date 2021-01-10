c6cfdd806e81dc359b08fcb4ef5915970078ee82
EdwardLynx24 <eduardodarkar24@gmail.com>
2021-01-10 10:28:21 -0600

    Primera insercion por request

--- app/Controllers/Http/PersonaController.js
+++ app/Controllers/Http/PersonaController.js
@@ -1,5 +1,6 @@
 'use strict'
 const Database = use('Database')
+const Persona = use ('App/Models/Persona')
 
 class PersonaController {
     async selectPersons(){
@@ -11,10 +12,23 @@ class PersonaController {
         return await Database.select('*').from('personas').where({id: id['id']})
     }
     async insertPersons({request, response}){
-        const persona = await Database
+        const {nombre, apellido_paterno, apellido_Materno, edad, sexo, correo, password}=request.all()
+        const personaNueva = new Persona()
+        personaNueva.fill({
+            nombre,
+            apellido_paterno,
+            apellido_Materno,
+            edad,
+            sexo,
+            correo,
+            password
+        })
+        await personaNueva.save()
+        return response.status(204).send("Persona ingresada papi")
+        /*const persona = await Database
         .table('personas')
-        .insert({nombre:'Eduardo',apellido_Paterno:'Maldonado',apellido_Materno:'Machuca',edad:'22',sexo:'Masculino',correo:'eduardodarkar02@gmail.com',password:'Edwardcr2'})
-        .returning('id')
+        .insert({nombre:'Eduardo',apellido_Paterno:'Maldonado',apellido_Materno:'Machuca',edad:'22',sexo:'Masculino',correo:'eduardodarkarDCR@gmail.com',password:'Edwardcr2'})
+        .returning('id')*/
     }
 }
 module.exports = PersonaController
